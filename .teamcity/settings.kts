949045189a7388a575f3391613d224261b6bd411
admin <admin@TeamCity>
2025-12-15 19:28:38 +0000

    Versioned settings configuration updated (TeamCity change in 'netology-test-20251215' project)

---
+++ .teamcity/settings.kts
@@ -0,0 +1,68 @@
+import jetbrains.buildServer.configs.kotlin.*
+import jetbrains.buildServer.configs.kotlin.buildFeatures.perfmon
+import jetbrains.buildServer.configs.kotlin.buildSteps.maven
+
+/*
+The settings script is an entry point for defining a TeamCity
+project hierarchy. The script should contain a single call to the
+project() function with a Project instance or an init function as
+an argument.
+
+VcsRoots, BuildTypes, Templates, and subprojects can be
+registered inside the project using the vcsRoot(), buildType(),
+template(), and subProject() methods respectively.
+
+To debug settings scripts in command-line, run the
+
+    mvnDebug org.jetbrains.teamcity:teamcity-configs-maven-plugin:generate
+
+command and attach your debugger to the port 8000.
+
+To debug in IntelliJ Idea, open the 'Maven Projects' tool window (View
+-> Tool Windows -> Maven Projects), find the generate task node
+(Plugins -> teamcity-configs -> teamcity-configs:generate), the
+'Debug' option is available in the context menu for the task.
+*/
+
+version = "2025.11"
+
+project {
+
+    buildType(Build)
+}
+
+object Build : BuildType({
+    name = "Build"
+
+    vcs {
+        root(DslContext.settingsRoot)
+    }
+
+    steps {
+        maven {
+            name = "Maven test"
+            id = "Maven2"
+
+            conditions {
+                doesNotEqual("teamcity.build.vcs.branch.NetologyTest20251215_HttpsGithubComWiqt8rExampleTeamcityGit", "refs/heads/master")
+            }
+            goals = "clean test"
+            runnerArgs = "-Dmaven.test.failure.ignore=true"
+        }
+        maven {
+            name = "Maven deploy (master)"
+            id = "Maven_deploy_master"
+
+            conditions {
+                equals("teamcity.build.vcs.branch.NetologyTest20251215_HttpsGithubComWiqt8rExampleTeamcityGit", "refs/heads/master")
+            }
+            goals = "clean deploy"
+            userSettingsSelection = "nexus-settings"
+        }
+    }
+
+    features {
+        perfmon {
+        }
+    }
+})
